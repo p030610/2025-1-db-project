6811909a4c5edcb46da049272099a1694bc224b1
Yeojun Yun <jjangyj8718@gmail.com>
2025-06-11 22:26:07 +0900

    sql: Edit bulkload sql of User table.

--- sql/user_create.sql
+++ sql/user_create.sql
@@ -3,3 +3,26 @@ VALUES ('ajou-user', '1234', 'user');
 
 INSERT INTO "User" (username, password_hash, role)
 VALUES ('ajou-admin', '1234', 'admin');
+
+INSERT INTO "User" (username, password_hash, role) VALUES
+('user001', '1234', 'user'),
+('minsu_kim', '1234', 'user'),
+('younghee2024', '1234', 'user'),
+('chulsoo_park', '1234', 'user'),
+('sujin_jung', '1234', 'user'),
+('donghyun99', '1234', 'user'),
+('soyoung_han', '1234', 'user'),
+('jaeho_yoon', '1234', 'user'),
+('jieun_lim', '1234', 'user'),
+('taewoo_kang', '1234', 'user'),
+('mirae_oh', '1234', 'user'),
+('hyunwoo_shin', '1234', 'user'),
+('jihye_bae', '1234', 'user'),
+('gildong_hong', '1234', 'user'),
+('mina_song', '1234', 'user'),
+('seongmin_jo', '1234', 'user'),
+('yerin_kwon', '1234', 'user'),
+('junho_seo', '1234', 'user'),
+('haneul_no', '1234', 'user'),
+('jihoon_moon', '1234', 'user'),
+('yujin_an', '1234', 'user');
